9ec271def3a14fa14fe24053d3ffd8ce729b8994
Mohammad Ali Yazdani <mohammadaliyazdani21@gmail.com>
2025-02-08 15:52:35 +0300

    feat: add big calendar component

---
+++ src/components/BigCalendar.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
+import timeGridPlugin from "@fullcalendar/timegrid";
+import { calendarEvents } from "@/lib/data";
+
+export default function BigCalendar() {
+  return (
+    <FullCalendar
+      slotMinTime="08:00:00"
+      slotMaxTime="17:00:00"
+      plugins={[dayGridPlugin, timeGridPlugin]}
+      initialView="timeGridWeek"
+      events={calendarEvents}
+      headerToolbar={{
+        start: "prev next",
+        end: "title timeGridWeek timeGridDay",
+      }}
+      height={1000}
+      contentHeight={1000}
+      expandRows={true}
+    />
+  );
+}
