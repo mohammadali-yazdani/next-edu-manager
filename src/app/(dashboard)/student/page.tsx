44ca469ce6fd61161c6c14d0483cf6ccd3077c5f
Mohammad Ali Yazdani <mohammadaliyazdani21@gmail.com>
2025-02-08 15:55:00 +0300

    feat: add components to student page

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,5 +1,28 @@
+import Announcements from "@/components/Announcements";
+import BigCalendar from "@/components/BigCalendar";
+import EventCalendar from "@/components/EventCalendar";
+
 const StudentPage = () => {
-  return <div className="">StudentPage</div>;
+  return (
+    <div className="p-4 flex gap-4 flex-col xl:flex-row">
+      {/* LEFT */}
+      <div className="w-full xl:w-2/3">
+        <div className="flex flex-col h-full bg-white p-4 rounded-md">
+          <div className="flex justify-between items-center">
+            <h1 className="text-xl font-semibold">Schedule (4A)</h1>
+          </div>
+          <div className="size-full">
+            <BigCalendar />
+          </div>
+        </div>
+      </div>
+      {/* RIGHT */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <EventCalendar />
+        <Announcements />
+      </div>
+    </div>
+  );
 };
 
 export default StudentPage;
