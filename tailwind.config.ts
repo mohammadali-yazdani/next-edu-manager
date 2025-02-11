f1acf32315c7f73b7aed1f28913bdb0244016848
Mohammad Ali Yazdani <mohammadaliyazdani21@gmail.com>
2025-02-11 10:07:53 +0300

    feat: install and configure tailwind scrollbar plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import scrollbar from "tailwind-scrollbar";
 
 export default {
   content: [
@@ -22,5 +23,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [scrollbar],
 } satisfies Config;
